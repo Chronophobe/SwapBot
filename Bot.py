4c154189396eedd809fc97e127121d1cdd1ef562
Stan Janssen <stanjanssen@outlook.com>
2014-11-26 22:04:09 +0000

    Database and bug fixes

--- Bot.py
+++ Bot.py
@@ -1,22 +1,31 @@
 # Python Bot Helper class by /u/FlockOnFire
-import praw, time, logging, requests, sys, math
+import praw, time, logging, requests, sys, math, sqlite3
 
 class Bot:
     def __init__(self, name, log_file, username = None, password = None, from_file = None, database = None):
-        logging.basicConfig(filename=log_file, level=logging.INFO)
+        logging.basicConfig(
+            filename=log_file,
+            level=logging.INFO,
+            format = '{asctime} | {levelname:^8} | {message}',
+            style='{'
+        )
         logging.getLogger().addHandler(logging.StreamHandler())
-
+        requests_logger = logging.getLogger('requests')
+        requests_logger.setLevel(logging.WARNING)
 
         self.name = name
         self.db = database
+        if self.db:
+            self.prepare_database()
+
         if from_file:
             self.reddit = self.login(from_file = from_file)
         else:
             self.reddit = self.login(username, password)
 
-        self.set_configurables()
         self.set_properties()
-
+        self.set_configurables()
+        
         init_msg = """
         Starting {0}...
             Username:            {1}
@@ -29,6 +38,14 @@ class Bot:
         self.refresh_rate = 10
         self.refresh_cap  = 120
         self.sub_from_subscriptions = False
+
+    def prepare_database(self):
+        cursor = self.db.cursor()
+        query = 'CREATE TABLE IF NOT EXISTS {} ({})'
+        cursor.execute(query.format(Comment.table, 'id VARCHAT NOT NULL'))
+        cursor.execute(query.format(Submission.table, 'id VARCHAT NOT NULL'))
+        self.db.commit()
+        pass
 
     def login(self, username = None, password = None, from_file = None):
         reddit = praw.Reddit('User-Agent: {0}'.format(self.name))
@@ -61,16 +78,20 @@ class Bot:
                 self.subreddits = ['FlockBots']
             for sub in self.subreddits:
                 self.check_comments(sub)
-                # self.check_submissions(sub)
-            # self.check_messages()
+                self.check_submissions(sub)
+            self.check_messages()
             time.sleep(Bot.sleep_time(self.idle_count, self.refresh_rate, self.refresh_cap))
             self.http_error_count = 0
         except requests.exceptions.HTTPError as e:
             self.http_error_count += 1
             if self.http_error_count > 5:
+                log.error('No connection available.')
                 raise EnvironmentError('No connection available.')
             else:
                 time.sleep(150)
+        except Exception as e:
+            logging.exception('Terminating due to error')
+            sys.exit()
 
     def set_properties(self):
         self.idle_count = 0
@@ -84,10 +105,6 @@ class Bot:
         return int(y)
 
     @staticmethod
-    def get_time():
-        return time.strftime("%d-%m-%Y %H:%M:%S")
-
-    @staticmethod
     def handle_ratelimit(func, *args, **kwargs):
         while True:
             try:
@@ -97,48 +114,48 @@ class Bot:
                 logging.warning('RateLimitExceeded | Sleeping {0} seconds'.format(error.sleep_time))
                 time.sleep(error.sleep_time)
 
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-class Database():
-    Base = declarative_base()
-    def __init__(self, file = 'bot.db'):
-        self.engine = create_engine('sqlite:///' + file, convert_unicode=True)
-        self.session = scoped_session(sessionmaker(autocommit=False,
-                                                 autoflush=False,
-                                                 bind=self.engine))
-        self.Base.query = self.session.query_property()
-        self.Base.metadata.create_all(bind=self.engine)
-
-class Comment(Database.Base):
-    __tablename__ = 'comments'
-    id = Column(String, primary_key=True)
-
-    def __init__(self, comment_id):
-        self.id = comment_id
+class Comment():
+    table = 'comments'
 
     @staticmethod
-    def add(id, session):
-        session.add(Comment(id))
-        session.commit()
+    def add(comment_id, db):
+        cursor = db.cursor()
+        try:
+            cursor.execute('INSERT INTO {} VALUES (?)'.format(Comment.table), (comment_id,))
+        except:
+            logging.exception('Could not add comment to database.')
+        else:
+            db.commit()
 
     @staticmethod
-    def is_parsed(id):
-        return Comment.query.filter(Comment.id == id).count() > 0
+    def is_parsed(comment_id, db):
+        cursor = db.cursor()
+        try:
+            cursor.execute('SELECT id FROM {} WHERE id = ?'.format(Comment.table), (comment_id,))
+        except:
+            logging.exception('Could not perform a SELECT query on Comments')
+        else:
+            return cursor.fetchone()
 
-class Submission(Database.Base):
-    __tablename__ = 'submissions'
-    id = Column(String, primary_key=True)
-
-    def __init__(self, submission_id):
-        self.id = submission_id
-
-    @staticmethod
-    def add(id, session):
-        session.add(Submission(id))
-        session.commit()
+class Submission():
+    table = 'submissions'
 
     @staticmethod
-    def is_parsed(id):
-        return Submission.query.filter(Submission.id == id).count() > 0
+    def add(submission_id, db):
+        cursor = db.cursor()
+        try:
+            cursor.execute('INSERT INTO {} VALUES (?)'.format(Comment.table), (submission_id,))
+        except:
+            logging.exception('Could not add submission to database.')
+        else:
+            db.commit()
+
+    @staticmethod
+    def is_parsed(submission_id, db):
+        cursor = db.cursor()
+        try:
+            cursor.execute('SELECT id FROM {} WHERE id = ?'.format(Comment.table), (submission_id,))
+        except:
+            logging.exception('Could not perform a SELECT query on Submissions')
+        else:
+            return cursor.fetchone()
