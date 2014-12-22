a6cba7eb7949cb2c8887dd133460d033f60d35e2
Stan Janssen <stanjanssen@outlook.com>
2014-12-22 11:03:09 +0000

    bug fix

--- Post.py
+++ Post.py
@@ -9,11 +9,11 @@ class Post():
         self.url      = post.permalink
         self.date     = date.fromtimestamp(post.created_utc).strftime('%Y%m%d')
         
-        if isinstance(self.post, praw.objects.Comment):
+        if isinstance(post, praw.objects.Comment):
             self.text  = post.body
             self.reply = post.reply
             self.title = post.submission.title
-        elif isinstance(self.post, praw.objects.Submission):
+        elif isinstance(post, praw.objects.Submission):
             self.text  = post.selftext
             self.reply = post.add_comment
             self.title = post.title
